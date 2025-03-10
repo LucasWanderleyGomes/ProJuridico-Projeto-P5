f3aabc7d903f2222c0ccd497cd99b811cd57529c
FelipeMaD <felipemarinhodr@hotmail.com>
2025-03-10 18:24:06 -0300

    Adicionando gitignore

--- backend/settings.py
+++ backend/settings.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lb1l$zuy7sz5r0%g_+%qpj7qxa!h2n2vp=q)7wzp^#43tu!$mk'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
