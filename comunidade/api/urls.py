0f9cc946d4afb699e6e096bad8d8f749d754bb8f
FelipeMaD <felipemarinhodr@hotmail.com>
2025-03-17 19:41:45 -0300

    criando comunidades com postagens específicas
    deixando as postagens exclusivas para cada comunidade

--- comunidade/api/urls.py
+++ comunidade/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ComunidadeViewSet
+from postagem.views import PostagensAPIView, PostagemAPIView
 
 router = DefaultRouter()
 router.register('comunidades', ComunidadeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('comunidades/<int:comunidade_pk>/postagens', PostagensAPIView.as_view() , name='comunidade_postagens'),
+    path('comunidades/<int:comunidade_pk>/postagens/<int:postagem_pk>', PostagemAPIView.as_view(), name='comunidade_postagem'),
 ]
--- comunidade/api/views.py
+++ comunidade/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from comunidade.models import Comunidade
 from .serializers import ComunidadeSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class ComunidadeViewSet(viewsets.ModelViewSet):
     queryset = Comunidade.objects.all()
     serializer_class = ComunidadeSerializer
+    
+   
