07fb7bed98cd54584e52961ed178595c462619fa
FelipeMaD <felipemarinhodr@hotmail.com>
2025-05-27 22:49:37 -0300

    [DOCUMENTAÇÃO] - swagger

--- postagem/api/serializers.py
+++ postagem/api/serializers.py
@@ -7,12 +7,12 @@ class PostagemSerializer(serializers.ModelSerializer):
     """
     Serializa postagens criadas por usuários em comunidades.
     Inclui curtidas, imagem e autor da publicação.
-    upload = serializers.ImageField(required=False, allow_null=True):
+    - upload = serializers.ImageField(required=False, allow_null=True):
         recebe o upload separadamente para tratamento
-    usuario = UserReturnSerializer(read_only=True):
+    - usuario = UserReturnSerializer(read_only=True):
         recebe o usuario separadamente para tratamento, relacionando o user à curtida
-    likes_count = serializers.SerializerMethodField()
-    has_liked = serializers.SerializerMethodField()
+    - likes_count = serializers.SerializerMethodField()
+        has_liked = serializers.SerializerMethodField()
         Recebe os atributos de like, para tratamento na api
 
     """
